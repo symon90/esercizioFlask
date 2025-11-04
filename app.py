13554960ea54a00e4bc9dcfc22fa76f355be3bcb
simone d amico <simodami90@hotmail.it>
2025-11-04 17:57:15 +0100

    installato vue.js e axios e testato la comunicazione tra frontend e backend

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from  flask import Flask, jsonify
 import requests
+from flask_cors import CORS
 
 
 BASE_URL='https://api.zeroc.green/v1/'
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/', methods=['GET'])
 def hello_world():
     response= requests.get(BASE_URL + 'stations/')
+    data= response.json()
+    stations=data.get('stations', [])
+    return jsonify(stations)
     
-    return response.json()
+
     
---
+++ client/src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+   <!-- <pre>{{ JSON.stringify(msg,null,2) }}</pre>
+     -->
+
+     {{ stationJson }}
+   <ul>
+    <li v-for="station in stationJson" :key="station.id">
+        {{ station.name }} - {{ station.address }}
+    </li>
+   </ul>
+
+</template>
+<script>
+
+import axios from 'axios';
+export default {
+    name: 'Home',
+    data() {
+        return {
+            stationJson: null,
+        }
+    },
+    methods: {
+        getJson() {
+            axios.get('http://localhost:5000/')
+                .then(response => {
+                    this.stationJson= response.data;
+                }).catch(error => {
+                    console.log(error);
+                });
+        }
+    },
+    created() {
+        this.getJson();
+    }
+
+}
+</script>
